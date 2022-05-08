2f6b33f7ad38c382d46e1e1408b71c08e43c031c
ylsama <ylongkaka3@gmail.com>
2022-05-09 02:40:27 +0700

    Update boilderplate video to aspectRatioImage

--- components/Layouts/LayoutPatterns/aspectRatioImageCard.js
+++ components/Layouts/LayoutPatterns/aspectRatioImageCard.js
@@ -1,11 +1,28 @@
-const AspectRatioImageCard = (title, descriptive) => {
+const AspectRatioImageCard = (title, descriptive, srcUrl) => {
     return (
-        <div class="grid place-items-center">
-            <div class="w-full sm:w-1/2 flex flex-col p-4 bg-gray-100 rounded-lg">
+        <div className="grid place-items-center">
+            <div
+                className="w-full sm:w-1/2 flex flex-col p-4 
+            bg-white rounded-lg overflow-hidden ring-1 ring-slate-900/5 shadow-xl"
+            >
                 <h1>{title ? 'Video Title' : title}</h1>
-                <div class="aspect-video bg-gray-200 rounded-lg"></div>
+                <div class="relative rounded-xl overflow-auto p-8">
+                    <iframe
+                        class="w-full aspect-video rounded-lg shadow-lg"
+                        src={
+                            srcUrl
+                                ? 'https://www.youtube.com/embed/dQw4w9WgXcQ'
+                                : srcUrl
+                        }
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen=""
+                    ></iframe>
+                </div>
                 <p>
-                    {descriptive ? 'Descriptive Text goes here' : descriptive}
+                    {descriptive
+                        ? 'Descriptive Text goes here - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed est error repellat veritatis.'
+                        : descriptive}
                 </p>
             </div>
         </div>
